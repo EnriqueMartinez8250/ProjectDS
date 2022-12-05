716f209d2d55155b25669f6ca8739d2c265cf3f2
Enrique Martinez-Acevedo <Enrique.martinez-acevedo@ucdenver.edu>
2022-12-05 16:26:15 -0700

    Added Main.cpp and Menu.h

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+// Project being made by Enrique Martinez-Acevedo, Karla Vela Lopez, Nicole Montenegro
+
+#include "Menu.h"
+#include <iostream>
+#include <string>
+
+
+int main() //Driver code for the program
+{
+    PrintMenu();    
+    //Comment test
+}
